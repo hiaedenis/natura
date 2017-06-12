a9a027f45d5785540ba683fc4572af7fe3b0694a
Dedê Silva <hiaedenis@gmail.com>
2017-06-12 10:30:32 -0300

    NewContent
    Updated

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*Modal Video*/
+$(".js-modal-btn").modalVideo();
+
+/*Carousel*/
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:4,
+    dots:false
+})
+});
